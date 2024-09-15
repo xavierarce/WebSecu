e31d4fa7db93ea7fb99088dfb2e25fb6b5001e95
Xavier Arce <xavierarce54321@gmail.com>
2024-09-15 20:50:05 +0200

    ✨ Created BackButton component

---
+++ src/components/BackButton/BackButton.tsx
@@ -0,0 +1,31 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Icon from "@/assets/icons";
+import { theme } from "@/src/constants/theme";
+import { useRouter } from "expo-router";
+
+const BackButton = ({ size = 26 }) => {
+  const router = useRouter();
+
+  return (
+    <Pressable onPress={() => router.back()} style={styles.button}>
+      <Icon
+        name="arrowLeft"
+        strokeWidth={2.5}
+        size={size}
+        color={theme.colors.text}
+      />
+    </Pressable>
+  );
+};
+
+export default BackButton;
+
+const styles = StyleSheet.create({
+  button: {
+    alignSelf: "flex-start",
+    padding: 5,
+    borderRadius: theme.radius.sm,
+    backgroundColor: "rgba(0, 0, 0, 0.07)",
+  },
+});
