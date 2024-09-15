68a4a162175b9e18193cacf6e88a88137b27a9d6
Xavier Arce <xavierarce54321@gmail.com>
2024-09-15 20:50:41 +0200

    ✨ Created Loading Component Component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,19 @@
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { theme } from "@/src/constants/theme";
+import { LoadingProps } from "./Loading.d";
+
+const Loading: React.FC<LoadingProps> = ({
+  size = "large",
+  color = theme.colors.primary,
+}) => {
+  return (
+    <View style={{ justifyContent: "center", alignContent: "center" }}>
+      <ActivityIndicator size={size} color={color} />
+    </View>
+  );
+};
+
+export default Loading;
+
+const styles = StyleSheet.create({});
