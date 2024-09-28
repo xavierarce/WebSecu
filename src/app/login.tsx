d8fd2d49a6cbf28168f8b7a31d7b22f3a5cbebf2
Xavier Arce <xavierarce54321@gmail.com>
2024-09-28 22:31:07 +0200

    ✨ Created login route

---
+++ src/app/login.tsx
@@ -0,0 +1,127 @@
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { theme } from "../constants/theme";
+import { StatusBar } from "expo-status-bar";
+import ScreenWrapper from "../components/ScreenWrapper";
+import BackButton from "../components/BackButton/BackButton";
+import { router, useRouter } from "expo-router";
+import { hp, wp } from "../helpers/common";
+import { useTranslation } from "react-i18next";
+import Input from "../components/Input/Input";
+import Icon from "@/assets/icons";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Button from "../components/Button/Button";
+
+const Login = () => {
+  const { t } = useTranslation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = () => {
+    if (!email || !password) {
+      Alert.alert(t("login-page.hey"), t("login-page.errors.missing_fields"));
+      return;
+    }
+    console.log(email, password);
+  };
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ScreenWrapper bg="white">
+        <StatusBar style="dark" />
+        <View style={styles.container}>
+          <BackButton />
+
+          <View>
+            <Text style={styles.welcomeText}>{t("login-page.hey")}</Text>
+            <Text style={styles.welcomeText}>{t("login-page.welcome")}</Text>
+          </View>
+
+          <View style={styles.form}>
+            <Text>{t("login-page.please_login")}</Text>
+            <Input
+              icon={
+                <Icon name="mail" size={26} strokeWidth={1.6} color="black" />
+              }
+              placeholder={t("login-page.form.email")}
+              onChangeText={(e) => setEmail(e)}
+            />
+            <Input
+              icon={
+                <Icon name="lock" size={26} strokeWidth={1.6} color="black" />
+              }
+              secureTextEntry
+              placeholder={t("login-page.form.password")}
+              onChangeText={(e) => setPassword(e)}
+            />
+            <Text style={styles.forgotPassword}>
+              {t("login-page.form.forgot_password")}
+            </Text>
+            <Button
+              title={t("login-page.form.login")}
+              loading={loading}
+              onPress={onSubmit}
+            />
+          </View>
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>
+              {t("login-page.footer.dont_have_account")}
+            </Text>
+            <Pressable
+              onPress={() => {
+                router.push("signup" as any);
+              }}
+            >
+              <Text
+                style={[
+                  styles.footerText,
+                  {
+                    color: theme.colors.primaryDark,
+                    fontWeight: theme.fonts.semibold,
+                  },
+                ]}
+              >
+                {t("login-page.footer.sign_up")}
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </ScreenWrapper>
+    </GestureHandlerRootView>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: wp(5),
+  },
+  welcomeText: {
+    fontSize: hp(4),
+    fontWeight: "bold",
+    color: theme.colors.text,
+  },
+  form: {
+    gap: 25,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    color: theme.colors.text,
+    fontWeight: theme.fonts.semibold,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    color: theme.colors.text,
+    textAlign: "center",
+    fontSize: hp(1.6),
+  },
+});
