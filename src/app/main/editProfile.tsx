c1d316d88a29e8b727132866bdd8d9721fb0c333
Xavier Arce <xavierarce54321@gmail.com>
2024-10-19 23:28:47 +0200

    ✨ Created profile route

---
+++ src/app/main/editProfile.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "@/src/components/ScreenWrapper";
+
+const EditProfie = () => {
+  return (
+    <ScreenWrapper>
+      <Text>EditProfie</Text>
+    </ScreenWrapper>
+  );
+};
+
+export default EditProfie;
+
+const styles = StyleSheet.create({});
