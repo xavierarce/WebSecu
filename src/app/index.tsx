df326a4d037499094e545527cccfcf89fa561127
Xavier Arce <xavierarce54321@gmail.com>
2024-10-20 23:09:33 +0200

    ⚰️ Dead code

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,10 @@
-import { View, Text, Button } from "react-native";
+import { View } from "react-native";
 import React from "react";
-import { router } from "expo-router";
 import ScreenWrapper from "../components/ScreenWrapper";
-import { useTranslation } from "react-i18next";
 import "../services/i18next";
 import Loading from "../components/Loading/Loading";
 
 const Index = () => {
-  const { t } = useTranslation();
-
   return (
     <ScreenWrapper>
       <View
